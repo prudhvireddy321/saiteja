514292a004bb0df700a6e89db890de67cda413c2
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-11 12:14:34 +0530

    Create roman.c

---
+++ roman.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+char str[];
+scanf("%s",&str);
+if(str=="I")
+printf(1);
+if(str=="II")
+printf(2);
+if(str=="III")
+printf(3);
+if(str=="IV")
+printf(4);
+if(str=="V")
+printf(s);
+if(str=="VI")
+printf(6);
+if(str=="VII")
+printf(7);
+if(str=="VIII")
+printf(8);
+if(str=="IX")
+printf(9);
+if(str=="X")
+printf(10);
+if(str=="XI")
+printf(11);
+}
