495e291fe0884d65b0676dd8802f03b447fe5cc0
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-27 14:06:48 +0530

    Create se.py

---
+++ se.py
@@ -0,0 +1,7 @@
+#check vowel or consonant
+import sys, string
+alpha = string.ascii_uppercase+string.ascii_lowercase
+#print(s)
+c = input('enter a char : ')
+if c in alpha : print('Alphabet')
+else  :  print('No')
