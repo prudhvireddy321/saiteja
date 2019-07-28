f1ae6b6309d29fcf7776038021b30646f991aeaa
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-28 15:43:45 +0530

    Update mm.py

--- mm.py
+++ mm.py
@@ -1,12 +1,12 @@
 import sys, string, math
-n = int(input())
-k = 2**n
+t = int(input())
+k = 2**t
 L = []
 for i in range(0,k) :
     s = bin(i)[2:]
     j = len(s)
-    if j < n :
-        s = '0' * (n-j) + s
+    if j < t :
+        s = '0' * (t-j) + s
     L.append(s)
 for i in range(0,len(L)) :
     print(L[i])
