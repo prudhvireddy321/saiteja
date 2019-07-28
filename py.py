e794964c1dab26a8b69c91f450f2d8fc80dfc838
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-28 16:03:20 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,13 +1,13 @@
 import sys, string, math
 s1,s2 = input().split()
-n1 = len(s1)
+n = len(s1)
 n2 = len(s2)
-if n2 > n1 :
+if n2 > n :
     i = 0
-    while i<n1 and s1[i] == s2[i] :
+    while i<n and s1[i] == s2[i] :
         i += 1
     print(n2-i)
-elif n2 == n1 :
+elif n2 == n :
     i = 0
     while i<n2 and s1[i] == s2[i] :
         i += 1
@@ -24,4 +24,4 @@ else :
         if c in L :
             k += 1
             L.remove(c)
-    print(n1-i-k)
+    print(n-i-k)
