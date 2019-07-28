47b9deff86735e826d4bf9ab3ed7a02ae2ef92a2
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-28 15:38:26 +0530

    Update fr.py

--- fr.py
+++ fr.py
@@ -1,11 +1,11 @@
 import sys, string, math
 
 def printMaxSubSquare(M):
-    R = len(M)  # no. of rows in M[][]
+    G = len(M)  # no. of rows in M[][]
     C = len(M[0])  # no. of columns in M[][]
-    S = [[0 for k in range(C)] for l in range(R)]
+    S = [[0 for k in range(C)] for l in range(G)]
 
-    for i in range(1, R):
+    for i in range(1, G):
         for j in range(1, C):
             if (M[i][j] == 1):
                 S[i][j] = min(S[i][j - 1], S[i - 1][j],
@@ -16,7 +16,7 @@ def printMaxSubSquare(M):
     max_of_s = S[0][0]
     max_i = 0
     max_j = 0
-    for i in range(R):
+    for i in range(G):
         for j in range(C):
             if (max_of_s < S[i][j]):
                 max_of_s = S[i][j]
